3013fc6d255d6e3cbe7e9228d9c2e3a65d431bb8
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-01 15:36:40 -0500

    adds posts request to server

--- calculator.js
+++ calculator.js
@@ -5,6 +5,10 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
+app.post("/", function(req, res) {
+    console.log("Thanks for posting that!")
+})
+
 app.listen(3000, function() {
     console.log("Server started on port 3000")
 })
