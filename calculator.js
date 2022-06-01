8b1c31c2b352bfb0967434590f748e0a48e96187
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-01 16:10:12 -0500

    adds bmi calculator

--- calculator.js
+++ calculator.js
@@ -5,14 +5,25 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.get("/bmi", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
 })
 
 app.post("/", function(req, res) {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
     var result = num1 + num2;
-    console.log("The result of the calculation is " + result);
+    res.send("The result of the calculation is " + result);
+})
+
+app.post("/bmi", function(req, res) {
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var result = weight / height / height;
+    res.send("Your bmi is " + result);
 })
 
 app.listen(3000, function() {
